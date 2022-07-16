fdec2182312adb75dceb3e56a4358a0a10d4a301
Ming Huan Yang <49542664+mhy1264@users.noreply.github.com>
2022-07-16 17:48:41 +0800

    finish frontend

--- src/pages/check.vue
+++ src/pages/check.vue
@@ -1,43 +1,50 @@
 <template>
+  <div class="q-pa-md">
     <div class="q-gutter-md" style="max-width: 300px">
-    <q-form ref="form" greedy class="q-gutter-y-md"  @keydown.enter.prevent >
-      <q-select
-
-        map-optionss
-        label="選取使用者"
-        :options="users"
-      />
-      <q-btn-group outline>
-        <q-btn outline label="上班" @click="onduty" />
-        <q-btn outline label="下班" @click="offduty" />
-        <q-btn outline label="打卡紀錄" @click="record" />
-      </q-btn-group>
-    </q-form>
+      <q-form ref="form" greedy class="q-gutter-y-md" @keydown.enter.prevent >
+        <q-select 
+            v-model="admin" 
+            label="請選取使用者" 
+            :options="users" 
+            option-label="AdminName" 
+            option-value="AdminId"
+            option-disable="inactive" 
+            emit-value 
+            map-options 
+            />
+        <q-btn-group outline>
+          <q-btn color="black" outline label="上班" @click="onduty" />
+          <q-btn color="black" outline label="下班" @click="offduty" />
+          <q-btn color="black" outline label="打卡紀錄" @click="record" />
+        </q-btn-group>
+      </q-form>
+      <q-btn outline label="click Me" @click="changeSignal" /> 
+      <q-input v-if="check" outline label="check"></q-input> 
     </div>
+  </div>
 </template>
 
-<script>
 
+
+<script>
 export default {
   data: () => ({
     name: "check",
-    info: {
-      label: '',
-      name: '',
+    admin:'',
+    check:{
+      status : false,
     },
-    users: [
-      {
-        label: "選取使用者",
-        value: ""
-      },
-      {
-        label: "Huan",
-        value: "Huan"
-      }
-
-    ]
+    users: []
   }),
   methods: {
+   loadData () {
+
+          this.users = data.value;
+    },
+    changeSignal(){
+      this.check = !this.check;
+      console.log(this.check);
+    },
     getValue() {
       return {
         id:this.user.label,
@@ -46,13 +53,26 @@ export default {
     },
     onduty() {
       console.log("上班")
+      // 接api -> Onduty.php
+      // 打卡上班
+      // reference : https://jeremysu0131.github.io/axios-%E8%99%95%E7%90%86-x-www-form-urlencoded-%E6%A0%BC%E5%BC%8F%E5%95%8F%E9%A1%8C/
+      // prama = user.id
+      this.record(); 
     },
     offduty() {
       console.log("下班")
+      // 接api -> Offduty.php
+      // 打卡下班
+      // prama = user.id
+      this.record()
     },
     record() {
+      this.changeSignal();
       console.log("打卡紀錄")
-
+      // 接api -> Log.php
+      // show 出table 資料
+      // prama = user.id
+      this.changeSignal();
     }
   }
 }
--- src/pages/query.vue
+++ src/pages/query.vue
@@ -1,22 +1,60 @@
 <template>
+  <div class="q-pa-md">
     <div class="q-gutter-md" style="max-width: 300px">
-      <q-select
-        label="Year"
-        :options="year"
-      />
-      <q-select
-        label="Month"
-        :options="month"
-      />
+      <q-form ref="form" greedy class="q-gutter-y-md" @submit="onSubmit" @keydown.enter.prevent>
+        <q-select v-model="form.y" label="Year" :options="year" option-label="label" option-value="value"
+          option-disable="inactive" emit-value map-options />
+        <q-select v-model="form.m" label="Month" value="form.m" :options="month" option-label="label"
+          option-value="value" option-disable="inactive" emit-value map-options />
+        <q-btn outlined  color="black"  label="查詢" flat type="submit"  />
+      </q-form>
     </div>
+  </div>
 </template>
 
 <script>
-
+var todayYear = (new Date().getFullYear()).toString();
+var todayMonth = (new Date().getMonth() + 1).toString();
 export default {
   data: () => ({
     name: "query",
-  })
+    form: {
+      m: todayMonth,
+      y: todayYear
+    },
+    year: [
+      { label: todayYear, value: todayYear },
+      { label: todayYear - 1, value: todayYear - 1 },
+      { label: todayYear - 2, value: todayYear - 2 },
+    ],
+    month: [
+      { label: "Jan", value: "1" },
+      { label: "Feb", value: "2" },
+      { label: "Mar", value: "3" },
+      { label: "Apr", value: "4" },
+      { label: "May", value: "5" },
+      { label: "Jun", value: "6" },
+      { label: "Jul", value: "7" },
+      { label: "Aug", value: "8" },
+      { label: "Sep", value: "9" },
+      { label: "Oct", value: "10" },
+      { label: "Nov", value: "11" },
+      { label: "Dec", value: "12" }
+    ]
+  }),
+  methods: {
+    getYM() {
+      return {
+        y: this.form.y,
+        m: this.form.m
+      }
+    },
+    onSubmit() {
+      const F = this.getYM();
+      // 串接api -> query.php
+      // param= F
+    }
+  }
 }
 
 </script>
